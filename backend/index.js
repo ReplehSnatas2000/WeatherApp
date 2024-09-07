b4350c8e017212fd53197bc06e044a057a6d4c9c
ReplehSnatas2000 <115682713+ReplehSnatas2000@users.noreply.github.com>
2024-09-07 12:50:41 +0530

    Updated port in index.js

--- backend/index.js
+++ backend/index.js
@@ -5,11 +5,12 @@ import dotenv from "dotenv";
 const app = express();
 app.use(cors());
 dotenv.config();
+const port = process.env.PORT || 3000;
 app.get("/weather", async (req, res) => {
     const city = req.query.city;
     const data = await getWeather(city);
     res.json(data);
 });
-app.listen(3000, () => {
-    console.log("Up and running on port 3000!!!");
+app.listen(port, () => {
+    console.log(`Up and running on port ${port}!!!`);
 });
